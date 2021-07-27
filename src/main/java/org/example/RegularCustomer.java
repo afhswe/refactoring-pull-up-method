392cdd1845393b22e67f38da771dd50a55d984ec
Andreas Hütter <andreas.huetter@nagarro.com>
2021-07-27 16:48:04 +0200

    refactor: remove pulled up method from first sub class to eliminate duplication;

--- src/main/java/org/example/RegularCustomer.java
+++ src/main/java/org/example/RegularCustomer.java
@@ -4,10 +4,6 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 
 public class RegularCustomer extends Customer {
-    public void createBill(LocalDateTime date) {
-        addBill(date, ChargeFor(lastBillDate(), date));
-    }
-
     protected double ChargeFor(LocalDateTime start, LocalDateTime end) {
         return (Duration.between(start, end)).toDays() * 100.0;
     }
